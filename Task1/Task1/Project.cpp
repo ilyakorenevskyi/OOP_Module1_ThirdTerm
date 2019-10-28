1e06f8f69572e89a07e77ae440f9d858c0066d27
ilyakorenevskyi <ilyakorenevskii@gmail.com>
2019-10-28 19:19:45 +0200

    fixed

--- Task1/Task1/Project.cpp
+++ Task1/Task1/Project.cpp
@@ -5,20 +5,17 @@ void Project::make_done(){
 	for (int i = 0; i != proj_develop.size(); i++) 
 		proj_develop[i].curr_proj = nullptr;
 }
-
 bool Project::check(){
 		int temp_sum=0;
 		for (int i = 0; i != dependence.size(); i++)
 			temp_sum += dependence[i].done;
 		return temp_sum == dependence.size();
 }
-
 void Project::add_worker(Developer worker){
 }
-
 Project::Project(){
 }
-Project::Project(int diff, std::vector<Tech> tech, std::vector<Developer> workers, std::vector<Project> projects) :difficulty(diff), proj_tech(tech), proj_develop(workers), dependence(projects) {}
+Project::Project(int diff, std::vector<Tech> tech, std::vector<Developer> workers, std::vector<Project> projects) :done(0), difficulty(diff), proj_tech(tech), proj_develop(workers), dependence(projects) {}
 
 Project::~Project()
 {
